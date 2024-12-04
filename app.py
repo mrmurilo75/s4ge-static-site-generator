263b1700a80ea4995887f4bcc01d3014c4419805
Murilo Rosa <67339072+mrmurilo75@users.noreply.github.com>
2024-12-04 17:00:52 +0000

    render template

--- app.py
+++ app.py
@@ -9,3 +9,25 @@ with open(SOURCE, 'r') as fin:
 
 with open(DESTINATION, 'w') as fout:  # Assumes folder exists
     fout.write(rendered)
+
+
+## Render template (Jinja2)
+from jinja2 import Environment, FileSystemLoader
+
+SOURCE = '_rendered/index.html'
+DESTINATION = '_site/index.html'
+
+TEMPLATES_FOLDER = '_templates/'
+TEMPLATE = 'index.html'
+
+env = Environment(loader=FileSystemLoader(TEMPLATES_FOLDER))
+template = env.get_template(TEMPLATE)
+
+context = {}
+with open(SOURCE, 'r') as fin:
+    context["content"] = fin.read()
+
+rendered = template.render(context)
+
+with open(DESTINATION, 'w') as fout:  # Assumes folder exists
+    fout.write(rendered)
