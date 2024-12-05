65074e2635ec750b66c45b990d4550a906f0b284
MrMurilo75 <mrmurilo75@gmail.com>
2024-12-05 19:54:56 +0000

    add task to copy resources

--- dodo.py
+++ dodo.py
@@ -2,3 +2,10 @@ def task_render_site(): # Task 'render_site'
     return {
         'actions': ['python3 app.py'],
     }
+
+def task_cp_resources(): # Task 'cp resources'
+    source_dir = '_source/'
+    destination_dir = '_site/'
+    return {
+        'actions': [f'rsync -a --exclude="*.md" --delete {source_dir}* {destination_dir}'],
+    }
