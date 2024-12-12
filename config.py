6cda5a1af96cbc90755954356975894d1ccb7710
MrMurilo75 <mrmurilo75@gmail.com>
2024-12-12 15:09:05 +0000

    refactor config

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 import json
 
 APP_DIR = Path(__file__).resolve(strict=True).parent
+PROJECT_DIR = APP_DIR
 
 # In the future we'll look for different extensions
-with (APP_DIR / '_config.json').open("r") as ff:
-    CONFIG = json.load(ff)
+with (PROJECT_DIR / "_config.json").open() as ff:
+    Configured = json.load(ff)
 
-SOURCE_PATH = APP_DIR / CONFIG["source"]
-DESTINATION_PATH = APP_DIR / CONFIG["destination"]
-TEMPLATES_PATH = APP_DIR / CONFIG["templates"]
+SOURCE_PATH = (PROJECT_DIR / Configured["source"])
+DESTINATION_PATH = PROJECT_DIR / Configured["destination"]
+
+Templates = Environment(loader=FileSystemLoader(PROJECT_DIR / Configured["templates"]))
