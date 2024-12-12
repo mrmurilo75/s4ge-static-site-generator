60e0d22f31afcf36f52e35801c95de4672a80950
MrMurilo75 <mrmurilo75@gmail.com>
2024-12-12 15:09:23 +0000

    use config import in dodo

--- dodo.py
+++ dodo.py
@@ -1,11 +1,13 @@
+from config import *
+
 def task_render_site(): # Task 'render_site'
     return {
         'actions': ['python3 app.py'],
     }
 
 def task_copy_resources(): # Task 'cp resources'
-    source_dir = '_source/'
-    destination_dir = '_site/'
+
+
     return {
-        'actions': [f'rsync -a --exclude="*.md" --delete {source_dir}* {destination_dir}'],
+        'actions': [f'rsync -a --checksum --exclude="*.md" --delete {SOURCE_PATH}* {DESTINATION_PATH}'],
     }
