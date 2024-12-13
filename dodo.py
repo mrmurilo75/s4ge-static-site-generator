e67b4bdb01e204daee2bf6ddd7d0435fc500ae3d
MrMurilo75 <mrmurilo75@gmail.com>
2024-12-13 19:27:35 +0000

    proper implement file_dep and targets in metadata

--- dodo.py
+++ dodo.py
@@ -1,24 +1,42 @@
 from s4ge import utils, app, config
 
+md_source = utils.get_all_files(config.SOURCE_PATH, utils.is_markdown)
+other_source = utils.get_all_files(
+    config.SOURCE_PATH, lambda s: not utils.is_markdown(s)
+)
+
+md_rendered = list(
+    map(
+        lambda s: s.with_suffix(".html"),
+        utils.replicate(
+            md_source, target=config.INTERMEDIARY_PATH, relative_to=config.SOURCE_PATH
+        ),
+    )
+)
+
 
 def task_render_md_to_html():
     return {
-        "file_dep": [*utils.get_all_files(config.SOURCE_PATH, utils.is_markdown)],
-        "targets": [config.Configured["rendered"]],
+        "file_dep": md_source,
+        "targets": md_rendered,
         "actions": [
-            (app.render_md_to_html, (config.SOURCE_PATH, config.Configured["rendered"]))
+            (app.render_md_to_html, (config.SOURCE_PATH, config.INTERMEDIARY_PATH))
         ],
     }
 
 
 def task_render_to_template():
     return {
-        "file_dep": [*utils.get_all_files(config.Configured["rendered"])],
-        "targets": [config.DESTINATION_PATH],
+        "file_dep": md_rendered,
+        "targets": utils.replicate(
+            md_rendered,
+            relative_to=config.INTERMEDIARY_PATH,
+            target=config.DESTINATION_PATH,
+        ),
         "actions": [
             (
                 app.render_to_template,
-                (config.Configured["rendered"], config.DESTINATION_PATH, "index.html"),
+                (config.INTERMEDIARY_PATH, config.DESTINATION_PATH, "index.html"),
             )
         ],
     }
@@ -26,12 +44,12 @@ def task_render_to_template():
 
 def task_copy_resources():  # Task 'cp resources'
     return {
-        "file_dep": [
-            *utils.get_all_files(
-                config.Configured["rendered"], lambda s: not utils.is_markdown(s)
-            )
-        ],
-        # "targets": [config.DESTINATION_PATH],  # ERROR: Two different tasks can't have a common target
+        "file_dep": other_source,
+        "targets": utils.replicate(
+            other_source,
+            relative_to=config.SOURCE_PATH,
+            target=config.DESTINATION_PATH,
+        ),
         "actions": [
             f'rsync -a --checksum --exclude="*.md" --delete {config.SOURCE_PATH}/* {config.DESTINATION_PATH}/'
         ],
--- s4ge/config.py
+++ s4ge/config.py
@@ -10,6 +10,7 @@ with (PROJECT_DIR / "_config.json").open() as ff:
     Configured = json.load(ff)
 
 SOURCE_PATH = PROJECT_DIR / Configured["source"]
+INTERMEDIARY_PATH = PROJECT_DIR / Configured["rendered"]
 DESTINATION_PATH = PROJECT_DIR / Configured["destination"]
 
 Templates = Environment(loader=FileSystemLoader(PROJECT_DIR / Configured["templates"]))
